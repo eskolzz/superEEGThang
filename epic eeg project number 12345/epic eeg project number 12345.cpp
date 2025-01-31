5171e02936be8f8e3eb7213ba16c39fb637f7d55
eskol <147757499+eskolzz@users.noreply.github.com>
2025-01-30 17:43:52 -0800

    Add files via upload

---
+++ epic eeg project number 12345/epic eeg project number 12345.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <iomanip>
+
+int main()
+{
+    int asdf;
+    std::cout << "=x=============== = : .   @@@@@@@@@@@@@@@@@@@    :====== =" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=o===============   @@@@@@@@@@@@@@@@@@@@@@@@@   ====== " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=r======+======   @@@@@@%@@@@@@*%@@@@@@@@@@@@@@  :==== " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=c===========:  @@@@@@@@@@@##@@- =@%@@@@@@%@@@@@  .=== " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=s=========-: =@@@@@@@@@@@@@=@@@..***@@@#@@@@@@@@@  := " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=r=========  @@@@@#@@@%@@@@@=#@@*.#*-+@@%%%#@@@@%@@: : " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=b========- @@@@*:.#@@*+@@@@=%-@%=+=-=-*+:=%+#%%*@@@   " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "+s======== .@@@@=--.=-= %@@%+=:=*=+=-==+=:.===##:@@@@  " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=6=======- @@@@%--**+:     :.::---+=-=-:.  .+-** @%@@  " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=4=======  @@@@=+@@@@@@@@@+==-==++++=-:=@@@@@@#+ @@@@@ " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=w======= @@@@@=:=-=-:-=#@@%#*++++++*@@@#::..::-.+@*@@ " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=e=--===- @@@@@.*=-.     :+###*******=-      :=+: @@+@ " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "+b+++++=--@@@@@.+#@@@@@@@#-=++++=++++-=@@@@@@%=+: @@%@ " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=s======::%@@@%.+*=--.@@.#@++*=+==-=-@@@@@=-+:++= @@@@ " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "=i===-==:   +@ -==-:.   .::-=++=-===-:.    .:---= @@@  " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "         -@@# ==+==+======+++*=================+ :@-  " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << ".t@@@@@@@@@@@@:=*+++++=+=+==+***===+====+++=+====+: @@@." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " =e@@%@@@+*#*##-+++==++===+=+*#++===+++=======+====- @**." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " %#+#@@%*#%#@:=*++++=======*%#+--===========++++=== @@#." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " -%@@@@@%%@@@: +*+++++==++=%*-=+=:. .==+=+========: *@+." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " @@@@@@@@@@@@@%=*+++++=====#%@@@@@%@@+++=+==++++=: *@@@:" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " @@@@@@###@@@@@@.#**+++++++==-::...:::==++=+=++=- %@@@@#" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " %@*@@@#@@%##%@@.=+++++++====+++===--===========: @@%%@%" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " :@+*@@-@#@@@%%@*.++*+++==--::----==-:::-====++= :@@@%+ " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " .@@%%@@@#%%@@@@@--+++++=+%@@@@@%**%@@@#=:-====. @@@@#@%" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "  *@@%@@@@@@@@@@@@ -=++**#@%*+*#%%#=--=#%%*===. @@**@@@." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " .+*@@@+##@@@@@@@@@=.:==++***=    ....:-====-:.@@#*@@*%:" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << " .#*%@%##@+@@@@%%@@@@==+****#@@@@@@@@@%*++++:*#*%@@*-+#:" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "   ..-##.*:-+=+*###@%@-.                     +::-::.:-:." << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+
+    cout << "\n\nMGMtNTUtMTQtMWMtNDUtMDUtMGYtMTMtMTMtMDMtNDgtMDUtMDAtMGItMWMtMDctMWYtNGMtMTEtMGMtMWMtNTItMGItNGEtNGYtMDYtMTEtMGItMWYtNTItMDUtMTEtMDEtNDUtMTktMGUtMWUtMDItNDUtNGUtMWYtMDEtMWEtMDUtNGMtMTQtMTgtMTUtMTgtNTktMWEtNWEtNGEtNDUtMWItMDEtNTItMTctMDgtMTgtMWQtNTItMDgtMGQtMDAtNDktMDgtMTAtMDItMDAtMGEtMTQtMWQtNTYtNGQtMjMtMmUtMTYtMTktMzgtMDEtMTgtMGQtNGEtMGItMWMtMWQtMWYtMjgtNGMtNGMtMGEtMjAtMDEtMzQtM2EtM2UtMmEtMGItMDktMTUtMDAtMWYtMzMtNmEtMGMtMDAtMzQtMDQtNWMtNTItMDgtMTYtMWYtNDUtMWItMGItNTQtMWEtMDMtNGUtMTMtMTgtNDktMDUtMDItMGQtMDItMTItMTQtMTAtMGUtNDctMDQtMWQtMTItMDQtMDYtMWYtMDAtNWYtNDUtMTQtMGEtMWQtNDgtMDctMWYtMTYtNGItMzAtMjEtMjItMzYtMjYtNDUtMTQtMWUtNWEtMTUtMTctNDUtMGUtMTMtMGItMDAtMTYtMWEtNDUtMTYtMGQtMGUtNDgtMTAtMDMtMGQtNGItMTEtMTgtMTYtMTItMWUtMGYtNTItMTEtNWEtMDYtNDA=" << endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    cout << "\n\nyou really thought i would give out my code for free so you could skid off of it? god, ill give you just one chance. Decrypt this string of text, and all I'm going to say is that there are 3 layers of encryption applied onto it. dm me on discord username eskolzz if you want a little hint ;). " << endl;
+    cout << "\n\n";
+
+    cin >> asdf;
+
+
+}
